48af7351de8c43e2a1ad746febef0a3b61e32047
Shiori Roy <shiori.roy@cognizant.com>
2024-05-01 06:38:31 +0000

    developing reports

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,10 +6,12 @@ o.shipdate,
 o.shipmode,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 --from customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 --from product
+p.productid,
 p.category,
 p.productname,
 p.subcategory 
