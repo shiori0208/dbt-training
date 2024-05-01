b61f756dde0af891dde21ee7131f3c5e37f6bff6
Shiori Roy <shiori.roy@cognizant.com>
2024-05-01 06:01:53 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+select 
+--from orders 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+--from customer
+c.customername,
+c.segment,
+c.country,
+--from product
+p.category,
+p.productname,
+p.subcategory 
+from {{ ref('raw_orders') }} as o 
+left join {{ ref('raw_customer') }} as c 
+on o.customerid = c.customerid 
+left join {{ ref('raw_product') }} as p 
+on o.productid = p.productid
